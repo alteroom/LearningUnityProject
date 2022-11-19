4f7ef24af858d534f1ee0d53309ded2d310776c7
Aleksandr Petrov <alteroom@gmail.com>
2022-11-19 21:57:49 +0700

    created visual, copied HP, damage and death mechanics

---
+++ Assets/Homeworks/1_GameMechanics/Scripts/Mechanics/DeathMechanics.cs
@@ -0,0 +1,33 @@
+
+using Homeworks._1_GameMechanics.Scripts.Primitives;
+using UnityEngine;
+
+namespace Homeworks._1_GameMechanics.Scripts.Mechanics
+{
+    public sealed class DeathMechanics : MonoBehaviour
+    {
+        [SerializeField]
+        private IntBehaviour hitPoints;
+
+        [SerializeField]
+        private EventReceiver deathReceiver;
+
+        private void OnEnable()
+        {
+            this.hitPoints.OnValueChanged += this.OnHitPointsChanged;
+        }
+
+        private void OnDisable()
+        {
+            this.hitPoints.OnValueChanged -= this.OnHitPointsChanged;
+        }
+
+        private void OnHitPointsChanged(int newHitPoints)
+        {
+            if (newHitPoints <= 0)
+            {
+                this.deathReceiver.Call();
+            }
+        }
+    }
+}
---
+++ Assets/Homeworks/1_GameMechanics/Scripts/Mechanics/TakeDamageMechanics.cs
@@ -0,0 +1,30 @@
+
+using Homeworks._1_GameMechanics.Scripts.Primitives;
+using UnityEngine;
+
+namespace Homeworks._1_GameMechanics.Scripts.Mechanics
+{
+    public sealed class TakeDamageMechanics : MonoBehaviour
+    {
+        [SerializeField]
+        private IntEventReceiver takeDamageReceiver;
+
+        [SerializeField]
+        private IntBehaviour hitPoints;
+
+        private void OnEnable()
+        {
+            this.takeDamageReceiver.OnEvent += this.OnDamageTaken;
+        }
+
+        private void OnDisable()
+        {
+            this.takeDamageReceiver.OnEvent -= this.OnDamageTaken;
+        }
+
+        private void OnDamageTaken(int damage)
+        {
+            this.hitPoints.Value -= damage;
+        }
+    }
+}
